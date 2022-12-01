f01f5ee4459de763cc1b97d3aa209408a177c56e
Josephine Mueni <nzi1900@byui.edu>
2022-12-01 04:01:33 -0800

    nav/footer/css

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+
+function toggleMenu(){
+    document.getElementById('navbtn').classList.toggle('open');
+    document.getElementById('hBtn').classList.toggle('open');
+  }
+  const x =document.getElementById("hBtn")
+  
+  x.onclick=toggleMenu;
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // footer last updated
+  document.querySelector('#last-updated').textContent =`Last-updated: ${document.lastModified}`;
+  
+  
+  
+  
+  
+  
+  
+    
